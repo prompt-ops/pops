0bc42360176b32ffa7b09c16ac3f558a8e0185ec
ytimocin <ytimocin@microsoft.com>
2025-01-11 16:24:01 -0800

    Adding a workflow for publishing the docs
    Signed-off-by: ytimocin <ytimocin@microsoft.com>

--- make/gendocs.mk
+++ make/gendocs.mk
@@ -1,5 +1,7 @@
+OUTPUT_PATH ?= docs
+
 .PHONY: generate-cli-docs
 generate-cli-docs:
 	@echo "Generating CLI docs for Prompt-Ops..."
-	@go run cmd/docgen/main.go docs
-	@echo "Generation complete."
+	@go run cmd/docgen/main.go $(OUTPUT_PATH)
+	@echo "Generation complete. Docs generated at $(OUTPUT_PATH)"
